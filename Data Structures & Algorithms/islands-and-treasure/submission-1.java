bab8d351431db639080edc4e602df147d623897d
Shounak99 <51371768+Shounak99@users.noreply.github.com>
2026-09-24 12:47:15 +0530

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+        Queue<int[]> bfs=new LinkedList<>();
+        int[][] dirs=new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
+         boolean[][] isVisited=new boolean[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==0){
+                    bfs.add(new int[]{i,j});
+                    isVisited[i][j]=true;
+            }
+        }
+    }
+         
+            
+                    while(!bfs.isEmpty()){
+                        int size=bfs.size();
+                        while(size>0){
+                            int[] curr=bfs.poll();
+                            for(int[] dir:dirs){
+                                int x=curr[0]+dir[0];
+                                int y=curr[1]+dir[1];
+                                if(x>=0 && x<grid.length && y>=0 && y<grid[0].length && grid[x][y]!=0 && grid[x][y]!=-1 && !isVisited[x][y]){
+                                    isVisited[x][y]=true;
+                                    grid[x][y]=Math.min(grid[x][y],grid[curr[0]][curr[1]]+1);
+                                    bfs.add(new int[]{x,y});
+                                }
+                            }
+                            size--;
+                        }
+                    }
+                }
+}
